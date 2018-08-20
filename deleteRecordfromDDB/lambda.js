f41c0f0df3704fd1cb4b36d2d58be61244e69ea3
TestAndunR <andunranmal@gmail.com>
2018-08-20 10:14:02 +0530

    delete oprtaion in ddb

--- deleteRecordfromDDB/lambda.js
+++ deleteRecordfromDDB/lambda.js
@@ -15,16 +15,16 @@ exports.handler = function (event, context, callback) {
         console.log(err)
     });
 
-    ddb.put({
+    ddb.delete({
         TableName: 'test03',
-        Item: { 'id1': 1, 'id2': '2', 'test': '03' }
+        Key: { 'id1': '1', 'id2': 2 }
     }).promise().then(function (data) {
         //your logic goes here
-        console.log(data)
     }).catch(function (err) {
         //handle error
-        console.log(err)
     });
+
+
 
 
     callback(null, 'Successfully executed');
